9bf5347787a882ccf5c33fc102452fde90dc6100
jdornemann <johann.dornemann.c@gmail.com>
2018-10-16 20:31:57 -0300

    Version Final Prueba

--- pruebaRuby.rb
+++ pruebaRuby.rb
@@ -1,26 +1,17 @@
 
-# David, 9, 2, 5,1, 1
-# Gonzalo, 10, 2,A, 8, 1
-# Mai, 10, 10, 9,9, A
-# JP, 10, 10, 10,10, 10
-
-
 def show_menu
 
-puts "Ingresar opción"
-puts "Opción 1:Generar archivo"
-puts "Opción 2: Inasistencias"
-puts "Opción 3: Alumnos aprobados"
-puts "Opción 4: Salir"
+puts 'Ingresar opción'
+puts 'Opción 1:Generar archivo'
+puts 'Opción 2: Inasistencias'
+puts 'Opción 3: Alumnos aprobados'
+puts 'Opción 4: Salir'
  
 return gets.chomp.to_i
-
 end
 
 def makeFileAVG
-
-    puts "aqui debes generar promedio"
- 
+  puts '################################################'
     lines = []
     
      File.open('archivo.cvs','r'){|file| lines = file.readlines}
@@ -39,64 +30,122 @@ def makeFileAVG
 
         File.open(nombre + '.cvs','w') do |write|
         
-            write.puts "Promedio:" + (suma/notas.length).to_s
+            write.puts 'Promedio:' + (suma/notas.length).to_s
        
         end
         # puts suma
         # puts notas.length
 
     end
+    puts 'Se ha generado el archivo con los promedios'
+    puts '################################################'
  validateOptionMenu
    
 end
 
 def totalAbsences
- puts "aquí hacer las inasistencias totales"
- validateOptionMenu
+ puts '################################################'
+
+
+    lines = []
+    
+    File.open('archivo.cvs','r'){|file| lines = file.readlines}
+        nombre = Array.new
+        lines.each do |line|
+            array_line = []
+            nombre = line.split(', ')[0]
+            absencesArray = line.split(', ').map(&:to_s)
+        
+            absences = absencesArray.count { |x| x.include? 'A' }
+
+
+            File.open(nombre + '.cvs','a') do |write|
+            
+                write.puts 'Inasistencias:' + absences.to_s
+        
+            end
+            puts 'Nombre Alumno:' + nombre + '  ' + 'Inasistencias:' + absences.to_s
+        end
+
+        puts '################################################' 
+    # puts suma
+    # puts notas.length
+    validateOptionMenu
 end
 
 def studentsPassed
 
-    puts "Aqui van los alumnos aprobados"
+    puts '################################################' 
+
+    lines = []
+    
+     File.open('archivo.cvs','r'){|file| lines = file.readlines}
+     nombre = Array.new
+    lines.each do |line|
+        array_line = []
+        nombre = line.split(', ')[0]
+        notas = line.split(', ').map(&:to_i)
+     
+        suma = 0
+        notas.delete(0)
+        notas.each do |x|
+            suma += x 
+    
+        end
+        
+        avg = (suma/notas.length)
+       
+        puts 'Alumno aprobado:' + nombre if GradeVerificationQualification(avg)
+
+        # puts suma
+        # puts notas.length
+
+    end
+
+
+    puts '################################################' 
+
     validateOptionMenu
 end
 
-def GradeVerificationQualification
+def GradeVerificationQualification(grade)
 
+    if grade <= 5
+        false 
+
+    else
+        true 
+    end
 end
 
 def validateOptionMenu
 
-    option = show_menu
+  option = show_menu
 
     while option != 0
 
-            case option
+      case option
             when 1
-                puts "soy 1"
-                makeFileAVG
+              puts 'Ha elegido la opción ' + option.to_s
+              makeFileAVG
             when 2
-                
-                puts "soy 2" 
-                totalAbsences
+              puts 'Ha elegido la opción ' + option.to_s
+              totalAbsences
 
             when 3
-                
-                puts "soy 3"
-                studentsPassed
-                
+              puts 'Ha elegido la opción ' + option.to_s
+              studentsPassed
             when 4 
-                puts "soy 4"
-                exit
-
+              puts '################################################' 
+              puts 'Ha salido del programa'
+              puts '################################################' 
+              exit
             else
-                puts "Opción ingresada no es valida."
-                validateOptionMenu
-            end
-
+              puts 'Opción ingresada no es valida.'
+              validateOptionMenu
+        end
     end
-
 end
 
-
+# BEGINING OF THE PROGRAM
 validateOptionMenu
